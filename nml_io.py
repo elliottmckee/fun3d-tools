f6365289ff3c470683a320922cbb5671ca22cc45
elliottmckee <emckee3779@gmail.com>
2024-11-02 11:43:19 -0700

    Simplifying handling of list-type nml inputs/outputs. Adding examples

--- nml_io.py
+++ nml_io.py
@@ -1,5 +1,5 @@
 '''
-IO for nml files
+IO for fun3d nml files
 
 Based around nested dictionary structure, 
 where each section (e.g., "raw_grid", "code_run_control") becomes a key in the outer dictionary, 
@@ -14,14 +14,9 @@ def format_bool(value):
 def format_float(value):
     abs_value = abs(value)
     if abs_value >= 1000 or abs_value < 0.001:
-        return f"{value:.4e}"  # Exponential notation for 'gross' floats
+        return f"{value:.6e}"  # Exponential notation for 'gross' floats
     else:
-        return f"{value:.4f}"  # Normal notation for 'nice' floats
-
-def list_write_helper(value):
-    # handles the parameter-side parenthesis for array quantities, i.e. schedule_cfl(1:2)
-    if isinstance(value, list): return f'(1:{len(value)})'
-    else: return ''
+        return f"{value:.6f}"  # Normal notation for 'nice' floats
         
 format_dict_write = { # Format specification for each of the possible types
     bool:   format_bool,
@@ -35,7 +30,8 @@ format_dict_write = { # Format specification for each of the possible types
 
 def nml_write(nml_data, out_filename, n_fixedwidth = 40):
 
-    os.makedirs(os.path.dirname(out_filename), exist_ok=True)
+    if '/' in out_filename:
+        os.makedirs(os.path.dirname(out_filename), exist_ok=True)
     
     with open(out_filename, 'w') as outfile:
 
@@ -45,7 +41,7 @@ def nml_write(nml_data, out_filename, n_fixedwidth = 40):
 
             # for each property in that block
             for property, value in block.items():
-                prop_full = property + list_write_helper(value)
+                prop_full = property
                 if len(prop_full) > n_fixedwidth: raise Exception('Property name too long for my dumb fixed-with implementation. Make smarter or increase n_fixedwidth') 
                 outfile.write('\t' + prop_full.ljust(n_fixedwidth) + ' = ' + format_dict_write[type(value)](value) + '\n')
 
@@ -54,46 +50,53 @@ def nml_write(nml_data, out_filename, n_fixedwidth = 40):
 
 def nml_read(filename):
 
-    nested_dict = {}
+    nml_dict = {}
     current_section = None
 
     with open(filename, 'r') as file:
         for line in file:
             line = line.strip()
+            
+            # find start of section
             if line.startswith('&'):
                 section_name = line.strip('&')
-                nested_dict[section_name] = {}
+                nml_dict[section_name] = {}
                 current_section = section_name
+            
+            # find end of section
             elif line.startswith('/'):
                 current_section = None
+            
+            # while in a section
             elif current_section:
                 key, value = line.split('=', 1)
-                key     = key.split('(', 1)[0].strip()
+                key     = key.strip()
                 value   = value.strip();
 
                 # If string
                 if value.startswith("'") and value.endswith("'"): 
-                    nested_dict[current_section][key] = value.strip("'")
+                    nml_dict[current_section][key] = value.strip("'")
 
                 # If bool
                 elif value.startswith(".") and value.endswith("."): 
-                    nested_dict[current_section][key] = value.strip(".").lower() == 'true'
+                    nml_dict[current_section][key] = value.strip(".").lower() == 'true'
                     
                 # If array
                 elif "," in value:
                     try: 
-                        nested_dict[current_section][key] = [int(x) for x in value.split(',')]
+                        nml_dict[current_section][key] = [int(x) for x in value.split(',')]
                     except ValueError:
-                        nested_dict[current_section][key] = [float(x) for x in value.split(',')]
+                        nml_dict[current_section][key] = [float(x) for x in value.split(',')]
 
                 # If scalar
                 else:
                     try: 
-                        nested_dict[current_section][key] = int(value)
+                        nml_dict[current_section][key] = int(value)
                     except ValueError:
-                        nested_dict[current_section][key] = float(value)
+                        nml_dict[current_section][key] = float(value)
          
-    return nested_dict
+    return nml_dict
+
 
 
 
